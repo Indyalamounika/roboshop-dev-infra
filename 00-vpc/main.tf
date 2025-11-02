e4be2c73073fe4c80b48767a9d334cf63bd4b431
Indyalamounika <vedamouni01@gmail.com>
2025-11-03 04:47:57 +0530

    terraform

--- 00-vpc/main.tf
+++ 00-vpc/main.tf
@@ -1,5 +1,5 @@
 module "vpc" {
-    source = "git::https://github.com/Indyalamounika/terraformawsvpc.git"
+    source = "git::https://github.com/Indyalamounika/terraformawsvpc.git?ref=main"
     # VPC
     vpc_cidr = var.vpc_cidr
     project_name = var.project_name
