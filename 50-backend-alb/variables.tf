df91a03d15c343776fb4be8b59c09f0ee6bd080c
Indyalamounika <vedamouni01@gmail.com>
2025-11-05 13:09:38 +0530

    terraform

---
+++ 50-backend-alb/variables.tf
@@ -0,0 +1,15 @@
+variable "project_name" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_id" {
+    default = "Z06796263L5WJ2A7XGU5G"
+}
+
+variable "domain_name" {
+    default = "mounikaindyala.fun"
+}
